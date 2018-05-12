876092c38a122d85d7ef3db9700a7d916bb00464
marvin evins <maevins@cps.edu>
2018-05-12 04:57:23 -0500

    defined backend
    defined backend with sql

---
+++ backend.py
@@ -0,0 +1,56 @@
+import sqlite3
+
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?,?,?,?) ",(title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows= cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title="", author="", year="", isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author = ? OR year = ? OR isbn = ?",(title, author, year, isbn))
+    rows= cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+
+def update(id,title, author, year, isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE * FROM book SET title=?,  author = ?,  year = ?, isbn = ?",( title, author, year, isbn,id))
+    cur.commit()
+    conn.close()
+
+
+connect()
+#insert("the sea","john tablet", 1918, 913123132)
+#delete(3)
+update(4, "the moon", "john smooth", 1917, 999999)
+print(view())
+
+print(search(author="John Smith"))
--- frontend.py
+++ frontend.py
@@ -14,6 +14,7 @@ close
 
 """
 from tkinter import *
+import backend
 
 window=Tk()
 
